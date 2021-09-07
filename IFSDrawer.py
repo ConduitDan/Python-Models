f397247240de5844535fbd585016536ee67558d5
Danny Goldstein <Dr.Danny.Goldstein@gmail.com>
2021-09-07 10:26:49 -0400

    IFSDrawer update
    Added support for IFS with non equal probability

--- IFSDrawer.py
+++ IFSDrawer.py
@@ -6,18 +6,27 @@ from matplotlib import pyplot as plt
 
 
 class IFSGenerator:
-    def __init__(self, IFS=None, depth=10, overwrite=False):
+    def __init__(self, IFS=None, depth=10, numPoints = None, overwrite=False):
         if IFS is None:
             raise "Please supply an IFS"
         self.IFS = IFS
         self.fileName = "fractals/" + IFS.name() + str(depth)
 
         self.overwrite = overwrite
-        self.depth = depth
         self.File = None
         self.functionList = self.IFS.getFunctionList()
         self.numOfFunctions = len(self.functionList)
-        self.numPoints = (self.numOfFunctions**(self.depth))
+        
+        
+        if numPoints is None:
+            self.depth = depth    
+            self.numPoints = (self.numOfFunctions**(self.depth+1))-1
+            
+        else:
+            self.numPoints = int(numPoints)
+            self.depth = int(np.ceil(np.log(self.numPoints)/np.log(self.numOfFunctions)))
+        
+            
         self.x = [0.0]*self.numPoints
         self.y = [0.0]*self.numPoints
         self.color = [[0,0,0]]*self.numPoints
@@ -45,7 +54,7 @@ class IFSGenerator:
         self.File.close()
         return (x,y)
 
-                
+                    
                 
     def generatePoints(self):
         if self.canReadFromFile():
@@ -58,9 +67,24 @@ class IFSGenerator:
                 xi = 0.0
                 yi = 0.0
                 colorsi = [0,0,0]
+                #currentMaxDepth = int(np.ceil(np.log(self.numPoints+1)/np.log(self.numOfFunctions)))-1
                 for j in range(self.depth):
                     # go through self.depth operations
-                    funIndex = int(i/(self.numOfFunctions**(j))) % self.numOfFunctions
+                    #funIndex = int(i/(self.numOfFunctions**(j))) % self.numOfFunctions
+                    
+                    #Create the function index by
+                    cumulitveP = 0
+                    funIndex = 0
+                    r = np.random.random()
+                    for p in self.IFS.getProbablities():
+                            cumulitveP += p
+                            if r > cumulitveP :
+                                funIndex += 1 
+                            else: 
+                                break
+                    
+                    
+                    
                     (xi,yi) = self.functionList[funIndex](xi,yi)
 #                    colorsi = [a + b/self.depth for a,b in zip(colorsi, self.IFS.getColors()[funIndex])]
                 self.x[i] = xi
@@ -108,8 +132,25 @@ class IFSGenerator:
         print("Plotting ...")
         self.plotFractal()
 
-class IFSGoldenDragon:
+class IFSAbstract:
+    def getColors(self):
+        return self.colorArray
+            
+    def getFunctionList(self):
+        return self.funArray
+    def name(self):
+        return self.myName
+    def getProbablities(self):
+        #by default use equal probablities
+        return [1/len(self.funArray)]*len(self.funArray)
+
+        
+
+class IFSGoldenDragon(IFSAbstract):
     def __init__(self):
+        
+        self.myName = "GoldenDragon"
+        
         phi = (1+np.sqrt(5))/2
         r = (1/phi)**(1/phi)
         theta = np.arccos((1+r**2 - r**4)/(2*r))
@@ -117,16 +158,15 @@ class IFSGoldenDragon:
         
         self.funArray = (afineTransfrom(theta, r),
                          afineTransfrom(theta2, r**2, np.array([[1.0],[0.0]])))
-                         
-    def getFunctionList(self):
-        return self.funArray
+        self.colorArray = ((0,0,0),(162.0/255,101.0/255,223.0/255))
+
+        
     
-    def name(self):
-        return "GoldenDragon"
-    
-    
-class IFSCustomDragon:
+class IFSCustomDragon(IFSAbstract):
     def __init__(self):
+        
+        self.myName = "CustomDragon"
+        
         theta1 = 1*np.pi/5
         theta2 = np.pi - theta1
 
@@ -135,37 +175,47 @@ class IFSCustomDragon:
         self.funArray = (afineTransfrom(theta1, scale1),
                          afineTransfrom(theta2, scale2, np.array([[1.0],[0.0]])))
         self.colorArray = ((0,0,0),(162.0/255,101.0/255,223.0/255))
-
-    def getFunctionList(self):
-        return self.funArray
-    def getColors(self):
-        return self.colorArray
         
-    def name(self):
-        return "CustomDragon"
 
-
-
-
+class IFSFern(IFSAbstract):
+    def __init__(self):
+        self.myName = "Fern"
+        f1 = afineTransfrom(R = np.array([[0,0],[0,0.16]]),translation = np.array([[0.0],[0.0]]))
+        f2 = afineTransfrom(R = np.array([[.85,0.04],[-0.04,0.85]]),translation = np.array([[0.0],[1.60]]))
+        f3 = afineTransfrom(R = np.array([[.2,-0.26],[0.23,0.22]]),translation = np.array([[0.0],[1.60]]))
+        f4 = afineTransfrom(R = np.array([[-.15,.28],[.26,0.24]]),translation = np.array([[0.0],[0.44]]))
+        self.funArray = (f1,f2,f3,f4)
+        self.colorArray = ((0,128/255,0),(0,64/255,0),(0,128/255,0),(0,128/255,0))
+        self.p = (.01,.85,.07,.07)
+    def getProbablities(self):
+        return self.p
+    
 
 class afineTransfrom:
-    def __init__(self,theta,scale,translation = np.array([[0.0],[0.0]])):
-        self.R = np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta),np.cos(theta)]]) * scale
+    def __init__(self,theta = None, R = None,scale = 1,translation = np.array([[0.0],[0.0]])):
+        if R is None:
+            self.R = np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta),np.cos(theta)]]) * scale
+        else:
+            self.R = R
+            
         self.t = translation
+
+
     def __call__(self,x,y):
         v = np.array([[x],[y]])
         v = np.matmul(self.R,v) + self.t
         return (v[0][0],v[1][0])
 
     
-        
+
 
 
 
 if __name__ == '__main__':
     #anIFS = IFSGoldenDragon()
-    anIFS = IFSCustomDragon()
-    myDrawBot = IFSGenerator(IFS = anIFS,depth = 15,overwrite = True)
+    #anIFS = IFSCustomDragon()
+    anIFS = IFSFern()
+    myDrawBot = IFSGenerator(IFS = anIFS,numPoints = 1e8,overwrite = True)
     myDrawBot.draw()
     
 
