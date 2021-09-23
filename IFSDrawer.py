5d75571c59c0fb3c1844a76f0aed32afce9b28e9
Danny Goldstein <Dr.Danny.Goldstein@gmail.com>
2021-09-23 10:08:38 -0400

    Decoupled Depth from NumPoints
    You can now create an IFS picture doesn't include all point in the attractor. useful for when you need to apply the functions many times before the picture takes shape

--- IFSDrawer.py
+++ IFSDrawer.py
@@ -2,11 +2,13 @@
 This class draws iterated function systems
 """
 import numpy as np
+import functools
+
 from matplotlib import pyplot as plt
 
 
 class IFSGenerator:
-    def __init__(self, IFS=None, depth=10, numPoints = None, overwrite=False):
+    def __init__(self, IFS=None, depth = None, numPoints = None, overwrite=False):
         if IFS is None:
             raise "Please supply an IFS"
         self.IFS = IFS
@@ -18,13 +20,17 @@ class IFSGenerator:
         self.numOfFunctions = len(self.functionList)
         
         
-        if numPoints is None:
+        if numPoints is None and depth is not None:
             self.depth = depth    
             self.numPoints = (self.numOfFunctions**(self.depth))
-            
-        else:
+        if depth is None and numPoints is not None:    
             self.numPoints = int(numPoints)
             self.depth = int(np.ceil(np.log(self.numPoints)/np.log(self.numOfFunctions)))
+        else:
+            self.depth = depth  
+            self.numPoints = int(numPoints)
+
+
         
             
         self.x = [0.0]*self.numPoints
@@ -46,27 +52,31 @@ class IFSGenerator:
         self.File = open(self.fileName+".out","r")
         xline = self.File.readline().split(',')
         yline = self.File.readline().split(',')
+        cline = self.File.readline().split(',')
+
         xline.pop() #remove /n
         yline.pop() #remove /n
+        cline.pop() #remove /n
         x = list(float(element) for element in xline)
         y = list(float(element) for element in yline) 
+        color = list(float(c) for c in cline)
 
         self.File.close()
-        return (x,y)
+        return (x,y,color)
 
                     
                 
     def generatePoints(self):
         if self.canReadFromFile():
-            (self.x,self.y) = self.readPointsFromFile()
+            (self.x,self.y,self.color) = self.readPointsFromFile()
         else:
             fractionDone = .1
-            numColors = 4
+            #numColors = 4
             for i in range(self.numPoints):
                 #for each possible combination 
                 xi = 0.0
                 yi = 0.0
-                colorsi = [0,0,0]
+                colorsi = 0
                 #currentMaxDepth = int(np.ceil(np.log(self.numPoints+1)/np.log(self.numOfFunctions)))-1
                 for j in range(self.depth):
                     # go through self.depth operations
@@ -85,13 +95,13 @@ class IFSGenerator:
                     
                     
                     
-                    (xi,yi) = self.functionList[funIndex](xi,yi)
-#                    colorsi = [a + b/self.depth for a,b in zip(colorsi, self.IFS.getColors()[funIndex])]
+                    (xi,yi) = self.functionList[funIndex]((xi,yi))
+                    colorsi += funIndex
                 self.x[i] = xi
                 self.y[i] = yi
-                colorProportion = int(numColors*(i/self.numPoints))/numColors
-                self.color[i] =  [a*colorProportion + b*(1.0-colorProportion) for a,b in zip(self.IFS.getColors()[0], self.IFS.getColors()[1])]
-                    
+                #colorProportion = int(numColors*(i/self.numPoints))/numColors
+                #self.color[i] =  [a*colorProportion + b*(1.0-colorProportion) for a,b in zip(self.IFS.getColors()[0], self.IFS.getColors()[1])]
+                self.color[i] = colorsi
                     
                 if i/self.numPoints > fractionDone:
                     print("%"+str(int(fractionDone*100)) + " Done")
@@ -104,7 +114,7 @@ class IFSGenerator:
         fig.set_size_inches(30,20)
         ax = fig.add_subplot(1,1,1)
         
-        ax.scatter(self.x,self.y,s = 1000/np.sqrt(self.numPoints), marker = ".",c = range(self.numPoints),cmap = 'hsv',edgecolors = None)
+        ax.scatter(self.x,self.y,s = 1000/np.sqrt(self.numPoints), marker = ".",c = self.color,cmap = 'summer',edgecolors = None)
         ax.set_aspect('equal')
         ax.xaxis.set_ticks([])
         ax.yaxis.set_ticks([])
@@ -118,9 +128,14 @@ class IFSGenerator:
             for xi in self.x:
                 self.File.write(str(xi)+',')
             self.File.write('\n')
-            print("%50 Done")
+            print("%33 Done")
             for yi in self.y:
                 self.File.write(str(yi)+',') 
+            self.File.write('\n')
+
+            print("%67 Done")
+            for c in self.color:
+                self.File.write(str(c)+',') 
             self.File.write('\n')
             self.File.close()
             
@@ -179,13 +194,24 @@ class IFSCustomDragon(IFSAbstract):
         
 
 class IFSFern(IFSAbstract):
-    def __init__(self):
-        self.myName = "Fern"
-        f1 = afineTransfrom(R = np.array([[0,0],[0,0.16]]),translation = np.array([[0.0],[0.0]]))
-        f2 = afineTransfrom(R = np.array([[.85,0.04],[-0.04,0.85]]),translation = np.array([[0.0],[1.60]]))
-        f3 = afineTransfrom(R = np.array([[.2,-0.26],[0.23,0.22]]),translation = np.array([[0.0],[1.60]]))
-        f4 = afineTransfrom(R = np.array([[-.15,.28],[.26,0.24]]),translation = np.array([[0.0],[0.44]]))
+    def __init__(self, D = 0, num = None):
+        
+        if num is None:
+            self.myName = "Fern"
+        else:
+            self.myName = "Fern" + str(num) + " "
+        angle = np.random.normal(np.sqrt(2*D))
+        R  = afineTransfrom(theta = angle)
+        RT = afineTransfrom(theta = -angle)
+        f1 = afineTransfrom(R = np.array([[ 0.00, 0.00],[ 0.00, 0.16]]),translation = np.array([[0.00],[0.00]]))
+        f2 = afineTransfrom(R = np.array([[ 0.85, 0.04+angle],[-0.04-angle, 0.85]]),translation = np.array([[0.00],[1.60]]))
+        f3 = afineTransfrom(R = np.array([[ 0.20,-0.26],[ 0.23, 0.22]]),translation = np.array([[0.00],[1.60]]))
+        f4 = afineTransfrom(R = np.array([[-0.15, 0.28],[ 0.26, 0.24]]),translation = np.array([[0.00],[0.44]]))
+        
+        
+        
         self.funArray = (f1,f2,f3,f4)
+        #self.funArray = list(compose(R,f,RT) for f in self.funArray)
         self.colorArray = ((0,128/255,0),(0,64/255,0),(0,128/255,0),(0,128/255,0))
         self.p = (.01,.85,.07,.07)
     def getProbablities(self):
@@ -202,22 +228,29 @@ class afineTransfrom:
         self.t = translation
 
 
-    def __call__(self,x,y):
-        v = np.array([[x],[y]])
+    def __call__(self,vin):
+        v = np.array([[vin[0]],[vin[1]]])
         v = np.matmul(self.R,v) + self.t
         return (v[0][0],v[1][0])
+        
+        
 
     
 
+def compose2(f,g):
+    return lambda *a, **kw: f(g(*a,**kw))
 
-
+def compose(*fs):
+    return functools.reduce(compose2, fs)
 
 if __name__ == '__main__':
     #anIFS = IFSGoldenDragon()
-    anIFS = IFSCustomDragon(theta =2* np.pi/3-.2)
-    #anIFS = IFSFern()
-    myDrawBot = IFSGenerator(IFS = anIFS,depth = 21,overwrite = False)
-    myDrawBot.draw()
+    #anIFS = IFSCustomDragon(theta =2* np.pi/3-.2)
+    for i in range(10):
+        anIFS = IFSFern(D = .001, num = i)
+        myDrawBot = IFSGenerator(IFS = anIFS,depth = 100, numPoints = 10e5, overwrite = True)
+        myDrawBot.draw()
+        
     
 
     
